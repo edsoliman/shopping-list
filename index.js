22ec2773bf8b2159af0d484ba541343ce8c226f6
esoliman0325 <43187571+esoliman0325@users.noreply.github.com>
2019-03-07 18:54:08 -0800

    Add files via upload
    completed shopping list app

---
+++ index.js
@@ -0,0 +1,21 @@
+$(function () {
+	$('ul').on('click', '.shopping-item-toggle', function(event) {
+		$(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+	});
+	$('ul').on('click', '.shopping-item-delete', function(event) {
+		$(this).closest('li').remove();
+	});
+	$('#js-shopping-list-form').submit(function(event) {
+		event.preventDefault();
+		let addedItem = $('#shopping-list-entry').val();
+		$('ul').append('<li>' +
+			'<span class="shopping-item">' + addedItem + '</span>' +
+			'<div class="shopping-item-controls">' +
+			'<button class="shopping-item-toggle">' +
+			'<span class="button-label">check</span>' +
+			'</button> ' +
+			'<button class="shopping-item-delete">' +
+			'<span class="button-label">delete</span>' +
+			'</button>' +
+			'</div></li>');
+})});
